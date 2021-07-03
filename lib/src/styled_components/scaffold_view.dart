3a705db84db63eddc32fd2c236102560445a769b
31Carlton7 <carltonaikins7@gmail.com>
2021-07-02 22:28:03 -0400

    Add appBar functionality for scaffold_view.dart

--- lib/src/styled_components/scaffold_view.dart
+++ lib/src/styled_components/scaffold_view.dart
@@ -3,12 +3,14 @@ import 'package:canton_design_system/canton_design_system.dart';
 class CantonScaffold extends StatelessWidget {
   final Widget? body;
   final Widget? bottomNavBar;
+  final AppBar? appBar;
   final EdgeInsets padding;
 
   const CantonScaffold({
     Key? key,
     this.body,
     this.bottomNavBar,
+    this.appBar,
     this.padding = const EdgeInsets.only(top: 17, left: 17, right: 17),
   }) : super(key: key);
   @override
@@ -17,6 +19,7 @@ class CantonScaffold extends StatelessWidget {
       onTap: () => CantonMethods.defocusTextfield(context),
       child: Scaffold(
         resizeToAvoidBottomInset: false,
+        appBar: appBar,
         bottomNavigationBar: bottomNavBar,
         body: SafeArea(
           child: Padding(
