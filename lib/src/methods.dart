00c413d600af2019a3c483650976ef7546440702
Carlton Aikins <carltonaikins7@gmail.com>
2021-10-03 22:35:03 -0400

    Update methods.dart

--- lib/src/methods.dart
+++ lib/src/methods.dart
@@ -2,12 +2,10 @@ import 'package:canton_design_system/canton_design_system.dart';
 
 class CantonMethods {
   /// Defocuses a textfield
-  static void defocusTextfield(BuildContext context) =>
-      FocusScope.of(context).requestFocus(new FocusNode());
+  static void defocusTextfield(BuildContext context) => FocusScope.of(context).requestFocus(new FocusNode());
 
   /// Default transition for switching views
-  static Future<void> viewTransition(BuildContext context, Widget view) =>
-      Navigator.push(
+  static Future<void> viewTransition(BuildContext context, Widget view) => Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) {
@@ -24,15 +22,12 @@ class CantonMethods {
   static String addDotsToString(String string, int index) {
     List<String> wordList = string.split(' ');
 
-    String result =
-        wordList.sublist(0, index).join(' ').replaceAll(RegExp(r' '), ' ') +
-            '...';
+    String result = wordList.sublist(0, index).join(' ').replaceAll(RegExp(r' '), ' ') + '...';
 
     return result;
   }
 
-  static String separateNumberByThreeDigits(
-      {required int source, bool? commas}) {
+  static String separateNumberByThreeDigits({required int source, bool? commas}) {
     RegExp reg = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
     String Function(Match) mathFunc;
 
@@ -53,8 +48,8 @@ class CantonMethods {
     return htmlText.replaceAll(exp, '');
   }
 
-  static Color alternateCanvasColor(BuildContext context, {int? index}) {
-    if (index != 1) return Theme.of(context).canvasColor;
+  static Color alternateCanvasColor(BuildContext context, {int? index, List<int?>? targetIndexes}) {
+    if (!targetIndexes!.contains(index) || index == null) return Theme.of(context).canvasColor;
 
     if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
       return Theme.of(context).canvasColor;
