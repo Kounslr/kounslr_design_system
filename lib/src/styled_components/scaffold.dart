674fe2a3c985216d81045aea5d6f38c67394e86a
Carlton Aikins <carltonaikins7@gmail.com>
2021-10-14 13:06:33 -0400

    Update scaffold.dart

--- lib/src/styled_components/scaffold.dart
+++ lib/src/styled_components/scaffold.dart
@@ -5,30 +5,42 @@ class CantonScaffold extends StatelessWidget {
   final Widget? bottomNavBar;
   final EdgeInsets? padding;
   final Color? backgroundColor;
-  final bool? resizeToAvoidBottomInset;
+  final bool? resizeToAvoidBottomInset, safeArea;
 
   const CantonScaffold({
     Key? key,
     this.body,
     this.bottomNavBar,
-    this.padding = const EdgeInsets.only(left: 17, right: 17),
+    this.padding = const EdgeInsets.symmetric(horizontal: 17),
     this.backgroundColor,
     this.resizeToAvoidBottomInset,
+    this.safeArea = true,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    Widget _body() {
+      if (safeArea!) {
+        return SafeArea(
+          child: Padding(
+            padding: padding ?? const EdgeInsets.symmetric(horizontal: 17),
+            child: body,
+          ),
+        );
+      } else {
+        return Padding(
+          padding: padding ?? const EdgeInsets.symmetric(horizontal: 17),
+          child: body,
+        );
+      }
+    }
+
     return GestureDetector(
       onTap: () => CantonMethods.defocusTextfield(context),
       child: Scaffold(
         resizeToAvoidBottomInset: resizeToAvoidBottomInset ?? false,
         bottomNavigationBar: bottomNavBar,
         backgroundColor: backgroundColor,
-        body: SafeArea(
-          child: Padding(
-            padding: padding ?? const EdgeInsets.only(left: 17, right: 17),
-            child: body,
-          ),
-        ),
+        body: _body(),
       ),
     );
   }
