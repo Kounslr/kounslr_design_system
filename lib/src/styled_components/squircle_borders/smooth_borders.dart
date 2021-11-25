1283bcba92e8d33041607529d5ebea876c427f2d
Carlton Aikins <carltonaikins7@gmail.com>
2021-11-25 12:24:38 -0500

    Update smooth_borders.dart

--- lib/src/styled_components/squircle_borders/smooth_borders.dart
+++ lib/src/styled_components/squircle_borders/smooth_borders.dart
@@ -1,24 +1,27 @@
 import 'package:canton_design_system/canton_design_system.dart';
 
 class CantonSmoothBorder {
-  static SmoothRectangleBorder defaultBorder() {
+  static SmoothRectangleBorder defaultBorder({BorderSide? side}) {
     return SmoothRectangleBorder(
+      side: side ?? BorderSide(color: CantonColors.transparent),
       borderRadius: SmoothBorderRadius.all(
         SmoothRadius(cornerRadius: 12, cornerSmoothing: 1),
       ),
     );
   }
 
-  static SmoothRectangleBorder largeBorder() {
+  static SmoothRectangleBorder largeBorder({BorderSide? side}) {
     return SmoothRectangleBorder(
+      side: side ?? BorderSide(color: CantonColors.transparent),
       borderRadius: SmoothBorderRadius.all(
         SmoothRadius(cornerRadius: 17, cornerSmoothing: 1),
       ),
     );
   }
 
-  static SmoothRectangleBorder smallBorder() {
+  static SmoothRectangleBorder smallBorder({BorderSide? side}) {
     return SmoothRectangleBorder(
+      side: side ?? BorderSide(color: CantonColors.transparent),
       borderRadius: SmoothBorderRadius.all(
         SmoothRadius(cornerRadius: 7, cornerSmoothing: 1),
       ),
@@ -30,8 +33,10 @@ class CantonSmoothBorder {
     double? topRight,
     double? bottomLeft,
     double? bottomRight,
+    BorderSide? side,
   }) {
     return SmoothRectangleBorder(
+      side: side ?? BorderSide(color: CantonColors.transparent),
       borderRadius: SmoothBorderRadius.only(
         topLeft: SmoothRadius(cornerRadius: topLeft ?? 0, cornerSmoothing: 1),
         topRight: SmoothRadius(cornerRadius: topRight ?? 0, cornerSmoothing: 1),
