6a1d2483d455eb30d39a6e09c8d3ce2fc71d8298
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-20 20:47:12 -0500

    Remove unneccessary imports

--- lib/src/styled_components/view_headers/view_header_two.dart
+++ lib/src/styled_components/view_headers/view_header_two.dart
@@ -1,5 +1,4 @@
 import 'package:canton_design_system/canton_design_system.dart';
-import 'package:canton_design_system/src/styled_components/buttons/null_button.dart';
 
 class ViewHeaderTwo extends StatelessWidget {
   final String? title;
@@ -27,18 +26,17 @@ class ViewHeaderTwo extends StatelessWidget {
             ? CantonBackButton(
                 isClear: isBackButtonClear ?? true,
                 onPressed: () {
-                  backButtonFunction != null
-                      ? backButtonFunction!()
-                      : DoNothingAction();
+                  backButtonFunction != null ? backButtonFunction!() : DoNothingAction();
                   Navigator.pop(context);
                 },
               )
             : buttonOne ?? CantonNullButton(),
         Text(
           title!,
-          style: Theme.of(context).textTheme.headline5!.copyWith(
-              color: textColor ?? Theme.of(context).colorScheme.primary,
-              fontWeight: FontWeight.w600),
+          style: Theme.of(context)
+              .textTheme
+              .headline5!
+              .copyWith(color: textColor ?? Theme.of(context).colorScheme.primary, fontWeight: FontWeight.w600),
         ),
         buttonTwo ?? CantonNullButton()
       ],
